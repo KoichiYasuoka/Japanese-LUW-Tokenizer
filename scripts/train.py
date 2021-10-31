cbb973f2b4560c9bac18679c16d436bb66ff51df
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2021-10-31 16:46:59 +0900

    Use "null" decoder

--- scripts/train.py
+++ scripts/train.py
@@ -1,18 +1,17 @@
 #! /usr/bin/python3 -i
 
 import unicodedata
-from tokenizers import SentencePieceUnigramTokenizer,trainers
-from tokenizers.pre_tokenizers import Whitespace
+from tokenizers import Tokenizer,models,pre_tokenizers,normalizers,trainers
 from transformers import RemBertTokenizerFast,AutoTokenizer
 tkz=AutoTokenizer.from_pretrained("KoichiYasuoka/bert-base-japanese-luw-upos")
 alp=[c for c in tkz.convert_ids_to_tokens([i for i in range(len(tkz))]) if len(c)==1 and unicodedata.name(c).startswith("CJK")]
 pst=tkz.backend_tokenizer.post_processor
-tkz=SentencePieceUnigramTokenizer(replacement=" ",add_prefix_space=False)
-tkz.pre_tokenizer=Whitespace()
-tkz.normalizer.handle_chinese_chars=False
-tkz.post_processor=pst
+tkz=Tokenizer(models.Unigram())
+tkz.pre_tokenizer=pre_tokenizers.Whitespace()
+tkz.normalizers=normalizers.Sequence([normalizers.Nmt(),normalizers.NFKC()])
 trn=trainers.UnigramTrainer(vocab_size=250300,special_tokens=["[PAD]","[UNK]","[CLS]","[SEP]","[MASK]","<special0>","<special1>","<special2>","<special3>","<special4>","<special5>","<special6>","<special7>","<special8>","<special9>"],initial_alphabet=alp,unk_token="[UNK]",max_piece_length=16,n_sub_iterations=2)
-tkz._tokenizer.train(files=["udja.luw.txt","aozora.luw.txt"],trainer=trn)
+tkz.train(files=["udja.luw.txt","aozora.luw.txt"],trainer=trn)
+tkz.post_processor=pst
 tkz.save("tokenizer.json")
 tokenizer=RemBertTokenizerFast(tokenizer_file="tokenizer.json",vocab_file="/dev/null",bos_token="[CLS]",cls_token="[CLS]",unk_token="[UNK]",pad_token="[PAD]",mask_token="[MASK]",sep_token="[SEP]",do_lower_case=False,keep_accents=True)
 tokenizer.save_pretrained("Japanese-LUW-Tokenizer")
