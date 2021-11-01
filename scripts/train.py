a3ac44fffb66fe4318d2492833998fc5b2dad9e1
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2021-11-01 10:31:29 +0900

    bug fix

--- scripts/train.py
+++ scripts/train.py
@@ -4,11 +4,11 @@ import unicodedata
 from tokenizers import Tokenizer,models,pre_tokenizers,normalizers,trainers
 from transformers import RemBertTokenizerFast,AutoTokenizer
 tkz=AutoTokenizer.from_pretrained("KoichiYasuoka/bert-base-japanese-luw-upos")
-alp=[c for c in tkz.convert_ids_to_tokens([i for i in range(len(tkz))]) if len(c)==1 and unicodedata.name(c).startswith("CJK")]
+alp=[c for c in tkz.convert_ids_to_tokens([i for i in range(len(tkz))]) if len(c)==1 and unicodedata.name(c).startswith("CJK UNIFIED")]
 pst=tkz.backend_tokenizer.post_processor
 tkz=Tokenizer(models.Unigram())
 tkz.pre_tokenizer=pre_tokenizers.Whitespace()
-tkz.normalizers=normalizers.Sequence([normalizers.Nmt(),normalizers.NFKC()])
+tkz.normalizer=normalizers.Sequence([normalizers.Nmt(),normalizers.NFKC()])
 trn=trainers.UnigramTrainer(vocab_size=250300,special_tokens=["[PAD]","[UNK]","[CLS]","[SEP]","[MASK]","<special0>","<special1>","<special2>","<special3>","<special4>","<special5>","<special6>","<special7>","<special8>","<special9>"],initial_alphabet=alp,unk_token="[UNK]",max_piece_length=16,n_sub_iterations=2)
 tkz.train(files=["udja.luw.txt","aozora.luw.txt"],trainer=trn)
 tkz.post_processor=pst
