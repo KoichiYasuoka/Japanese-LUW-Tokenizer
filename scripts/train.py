be6a60a7d86cc6701a5c647fd9a3b2547201e722
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2021-11-03 00:50:20 +0900

    decoder changed

--- scripts/train.py
+++ scripts/train.py
@@ -12,7 +12,7 @@ tkz.normalizer=normalizers.Sequence([normalizers.Nmt(),normalizers.NFKC()])
 trn=trainers.UnigramTrainer(vocab_size=250300,special_tokens=["[PAD]","[UNK]","[CLS]","[SEP]","[MASK]","<special0>","<special1>","<special2>","<special3>","<special4>","<special5>","<special6>","<special7>","<special8>","<special9>"],initial_alphabet=alp,unk_token="[UNK]",max_piece_length=16,n_sub_iterations=2)
 tkz.train(files=["udja.luw.txt","aozora.luw.txt","aug.luw.txt"],trainer=trn)
 tkz.post_processor=pst
-tkz.decoder=decoders.BPEDecoder(suffix="")
+tkz.decoder=decoders.WordPiece(prefix="",cleanup=True)
 tkz.save("tokenizer.json")
 tokenizer=RemBertTokenizerFast(tokenizer_file="tokenizer.json",vocab_file="/dev/null",bos_token="[CLS]",cls_token="[CLS]",unk_token="[UNK]",pad_token="[PAD]",mask_token="[MASK]",sep_token="[SEP]",do_lower_case=False,keep_accents=True)
 tokenizer.save_pretrained("Japanese-LUW-Tokenizer")
