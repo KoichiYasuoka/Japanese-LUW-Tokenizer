e35bd7ff7a3cd38ac07ab1ef57a81d617c508878
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2021-11-03 13:40:02 +0900

    copy supar.model

--- scripts/esupar.sh
+++ scripts/esupar.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
-
-exec biaffine-dep train -b -d 0 -c biaffine-dep-en -p supar.model -f bert --bert KoichiYasuoka/roberta-small-japanese-luw-upos --embed= --train train.conllu --dev UD_Japanese-GSDLUW/*-dev.conllu --test UD_Japanese-GSDLUW/*-test.conllu
+BERT=KoichiYasuoka/roberta-small-japanese-luw-upos
+biaffine-dep train -b -d 0 -c biaffine-dep-en -p supar.model -f bert --bert $BERT --embed= --train train.conllu --dev UD_Japanese-GSDLUW/*-dev.conllu --test UD_Japanese-GSDLUW/*-test.conllu
+cp -p supar.model $BERT/supar.model
+exit 0
